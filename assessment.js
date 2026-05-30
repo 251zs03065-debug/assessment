209c016ca7eed89f875246ab2d7ddf510723ce58
hach <251zs03065@student.zen.ac.jp>
2026-05-30 22:29:18 +0900

    CSS_Bootstrap
    CSSでデザインを整える

--- assessment.js
+++ assessment.js
@@ -18,15 +18,28 @@ assessmentButton.addEventListener( //イベント検知設定の追加
     }
 
     // TO DO 診断結果表示のエリアの作成 ＊id="result-area"div内に子要素として診断結果表示のHTMLが出現する
+     // headerDivision の作成
     resultDivision.innerText = ''; // divタグを空文字列で上書きリセットする、これによって診断結果が連続表示する現象を防ぐことができる
-    const header = document.createElement('h3');
-    header.innerText = '診断結果';
-    resultDivision.appendChild(header);
+    const headerDivision = document.createElement('div');
+    headerDivision.setAttribute('class', 'card-header text-bg-primary');
+    headerDivision.innerText = '診断結果';
+
     // 診断結果のassessment関数が実行され配列の結果が表示される
+    // bodyDivision の作成
+    const bodyDivision = document.createElement('div');
+    bodyDivision.setAttribute('class', 'card-body');
     const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', 'card-text');
     const result = assessment(userName);
     paragraph.innerText = result;
-    resultDivision.appendChild(paragraph);
+    bodyDivision.appendChild(paragraph);
+
+    // resultDivision に Bootstrap のスタイルを適用する
+    resultDivision.setAttribute('class', 'card');
+
+    // headerDivision と bodyDivision を resultDivision に差し込む
+    resultDivision.appendChild(headerDivision);
+    resultDivision.appendChild(bodyDivision);
 
     // TO DO ツイートエリアの作成
     tweetDivision.innerText = '';
